8eeb8534140a7bf9ca4d392a6429376020ae0d72
Parker Tuck <48261368+parkertuck@users.noreply.github.com>
2019-03-07 19:26:15 -0600

    Update proj1_2.cpp
    Updated Processor functions

--- proj1_2.cpp
+++ proj1_2.cpp
@@ -1,5 +1,5 @@
 /*
-  Names:        Parker Tuck, Adam Bell
+  Names:        Parker Tuck,
   Class:        CSCE 4600 - Operating Systems
   Instructor:   Armin R. Mikler
   Description:  This .cpp file should only do problem #1 as of right now
@@ -32,21 +32,17 @@ private:
 	  struct node *next;
 	};
 	struct node *headPtr, *tailPtr;
-	int numOfItems, avg_time, total_time, mem_avail;
+	int numOfItems, avg_time, total_time, mem_avail, processNum;
 
 public:
 
   Processor();
-//  ~Processor();
+  ~Processor();
   void add(const Process &);
   void remove();
   void print();
-  void setP1MemAvail();   // should be 2 GB
-  void setP2MemAvail();   // Shoudl be 2 GB
-  void setP3MemAvail();   // should be 4 GB
-  void setP4MemAvail();   // should be 4 GB
-  void setP5MemAvail();   // should be 8 GB
-	
+  void setMemAvail(int memory);   // should be 2 GB
+  void setProcessNum(int num);
 };
 
 Processor::Processor() {
@@ -56,16 +52,17 @@ Processor::Processor() {
   this->avg_time = 0;
   this->total_time = 0;
   this->mem_avail = 0;
+  this->processNum = 0;
 }
 
-/*
+
 Processor::~Processor() {
    int idx = 0;
   for ( idx = 0; idx < numOfItems; idx++ ) {
-    
+	  this->remove();
   }
 }
-*/
+
 
 /*
   Adds node to processor queue, no specific order in which the processes
@@ -89,7 +86,7 @@ void Processor::add(const Process &processes) {
 	this->tailPtr->next = newNodePtr;
 	this->tailPtr = newNodePtr;
 	this->numOfItems++;
-  
+
 }
 
 /*
@@ -97,40 +94,23 @@ void Processor::add(const Process &processes) {
 void Processor::remove() {
   if ( numOfItems == 0 )
     return;
+
+  // remove head node, assign next node as head, delete old head, decrease number of items in queue
+    struct node *tempNode = this->headPtr;	// tempNode = node being removed
+    this->headPtr = tempNode->next;			// assign the next node in queue as the headPtr
+    delete tempNode;						// delete the old head node
+    this->numOfItems--;						// decrease number of items in queue by one
 }
 
 /*
   Set memory available in this processor to 2 GB or 2000 MB
 */
-void Processor::setP1MemAvail() {
-  this->mem_avail = 2000;   // 2000 MB = 2 GB
-}
-/*
-  Set memory available in this processor to 2 GB or 2000 MB
-*/
-void Processor::setP2MemAvail() {
-  this->mem_avail = 2000;   // 2000 MB = 2 GB
+void Processor::setMemAvail(int memory) {
+  this->mem_avail = memory;   // 2000 MB = 2 GB, 4000 MB = 4 GB, or 8000 MB = 8 GB
 }
 
-/*
-  Set memory available in this processor to 4 GB or 4000 MB
-*/
-void Processor::setP3MemAvail() {
-  this->mem_avail = 4000;   // 4000 MB = 4 GB
-}
-
-/*
-  Set memory available in this processor to 4 GB or 4000 MB
-*/
-void Processor::setP4MemAvail() {
-  this->mem_avail = 4000;   // 4000 MB = 4 GB
-}
-
-/*
-  Set memory available in this processor to 8 GB or 8000 MB
-*/
-void Processor::setP5MemAvail() {
-  this->mem_avail = 8000;   // 8000 MB = 8 GB
+void Processor::setProcessNum(int num) {
+	this->processNum = num;		// either 1, 2, 3, 4, or 5
 }
 
 /*
@@ -140,14 +120,14 @@ void Processor::print() {
     cout << "No processes in processor.\n";
     return;
   }
-  
+
   struct node *tempPtr;
   tempPtr = this->headPtr;
   while ( tempPtr != NULL ) {
     cout << tempPtr->burst_val << endl;
     tempPtr = tempPtr->next;
   }
-  cout << "total time: " << this->total_time << endl;
+  cout << "total time " << this->processNum << ": " << this->total_time << endl;
 }
 
 int findBurstTime();
@@ -156,28 +136,31 @@ double findMemSize();
 int main() {
 
   Processor P1, P2, P3, P4, P5;
-  P1.setP1MemAvail(); P2.setP2MemAvail(); P3.setP3MemAvail(); P4.setP4MemAvail(); P5.setP5MemAvail();
+  P1.setProcessNum(1); P2.setProcessNum(2); P3.setProcessNum(3); P4.setProcessNum(4); P5.setProcessNum(5);
+  P1.setMemAvail(2000); P2.setMemAvail(2000); P3.setMemAvail(4000); P4.setMemAvail(4000); P5.setMemAvail(8000);
+
   struct Process p[NPROCESSES+1];
+
   int idx = 0, burstTotal = 0, burst_avg = 0;
   srand(time(NULL));
-  
+
   for ( idx = 0; idx <= NPROCESSES; idx++ ) {
     p[idx].pid = idx;
     p[idx].burst_time = findBurstTime();  // change value later
     p[idx].mem_size = findMemSize()/100;    // change value later
     P1.add(p[idx]);     // this is how you add processes to processor
   }
-  
+
   /*
   for (idx = 0; idx <+ NPROCESSES; idx++) {
     burstTotal += p[idx].burst_time;
     cout << p[idx].burst_time << endl;
   }*/
-  
+
   //burst_avg = burstTotal / 200;
   //cout << endl << burst_avg << endl << endl;
-  P1.print();
-  
+  P1.print(); P2.print(); P3.print(); P4.print(); P5.print();
+
   return 0;
 }
 
@@ -186,14 +169,14 @@ int main() {
 */
 int findBurstTime() {
   // generating a few negative numbers, FIX LATER
-  return rand() % (49999990000000) + (10000000); 
+  return rand() % 4999999 + 1;
 }
 
 /*
   Returns a random number to each unique process between .25 MB - 8 GB
 */
 double findMemSize() {
-  return rand() % 799975 + 25 ; // number returned is divided by 100 
+  return rand() % 799975 + 25 ; // number returned is divided by 100
                                 // to convert it back to MB
 }
 
