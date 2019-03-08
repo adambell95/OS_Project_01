7a2482575023adc6e55c7a45b998584c2446e1e0
Parker Tuck <48261368+parkertuck@users.noreply.github.com>
2019-03-08 00:50:41 -0600

    Update proj1_2.cpp

--- proj1_2.cpp
+++ proj1_2.cpp
@@ -9,8 +9,8 @@
 #include <stdlib.h>
 #include <ctime>
 
-#define NPROCESSES 10  // number of proceses required
-#define MAXPROCESSES 40 // might take out later
+#define NPROCESSES 200  // number of proceses required
+#define NPROCESSORS 5	// number of processors required
 
 using namespace std;
 
@@ -41,9 +41,10 @@ public:
   void add(const Process &);
   void remove();
   void print();
-  void setMemAvail(int memory);   // should be 2 GB
+  void setMemAvail(int memory);   // should be 2, 4, or 8 GB
   void setProcessNum(int num);
   bool isOverAvgTime(int avg_turnaround_time);
+  int getTotalTime();
 
 };
 
@@ -81,7 +82,7 @@ void Processor::add(const Process &processes) {
 		this->headPtr = newNodePtr;
 		this->tailPtr = newNodePtr;
 		this->numOfItems++;
-    this->total_time += processes.burst_time;
+		this->total_time += processes.burst_time;
 		return;
 	}
 
@@ -98,10 +99,10 @@ void Processor::remove() {
     return;
 
   // remove head node, assign next node as head, delete old head, decrease number of items in queue
-    struct node *tempNode = this->headPtr;	// tempNode = node being removed
-    this->headPtr = tempNode->next;			// assign the next node in queue as the headPtr
-    delete tempNode;						// delete the old head node
-    this->numOfItems--;						// decrease number of items in queue by one
+  struct node *tempNode = this->headPtr;	// tempNode = node being removed
+  this->headPtr = tempNode->next;			// assign the next node in queue as the headPtr
+  delete tempNode;						// delete the old head node
+  this->numOfItems--;						// decrease number of items in queue by one
 }
 
 /*
@@ -133,68 +134,112 @@ void Processor::print() {
   struct node *tempPtr;
   tempPtr = this->headPtr;
   while ( tempPtr != NULL ) {
-    cout << tempPtr->burst_val << endl;
+    //cout << tempPtr->burst_val << endl;
     tempPtr = tempPtr->next;
   }
-  cout << "total time " << this->processNum << ": " << this->total_time << endl;
+  cout << "Turnaround Time of Processor " << this->processNum << ": " << this->total_time << endl;
+}
+
+/*
+ *
+ */
+int Processor::getTotalTime() {
+	return this->total_time;
 }
 
 int findBurstTime();
 double findMemSize();
+void sortProcessesByTime(struct Process processArray[], int arraySize);
 
 int main() {
 
-  Processor P1, P2, P3, P4, P5;
-  P1.setProcessNum(1); P2.setProcessNum(2); P3.setProcessNum(3); P4.setProcessNum(4); P5.setProcessNum(5);
-  P1.setMemAvail(2000); P2.setMemAvail(2000); P3.setMemAvail(4000); P4.setMemAvail(4000); P5.setMemAvail(8000);
+  Processor P[NPROCESSORS];
+  for ( int idx = 0; idx < NPROCESSORS; idx++ ) {
+	P[idx].setProcessNum(idx+1);
+	if ( idx == 0 || idx == 1 )
+		P[idx].setMemAvail(2000);
+	else if ( idx == 2 || idx == 3 )
+		P[idx].setMemAvail(4000);
+	else
+		P[idx].setMemAvail(8000);
+  }
 
   struct Process p[NPROCESSES+1];
 
-  int idx = 0, burstTotal = 0, burst_avg = 0, counter = 1, temp_burst = 0;;
-  srand(time(NULL));
+  int idx = 0, burstTotal = 0, burst_avg = 0, temp_burst = 0;;
+  //srand(time(NULL));
+  srand(1);
 
-  for ( idx = 0; idx <= NPROCESSES; idx++ ) {
+  for ( idx = 0; idx < NPROCESSES; idx++ ) {
     p[idx].pid = idx;
 	temp_burst = findBurstTime();
-	p[idx].burst_time = temp_burst;  // change value later
+	p[idx].burst_time = temp_burst;  // change value later; Value * 10^7
 	burstTotal += temp_burst;
 	p[idx].mem_size = findMemSize()/100;    // change value later
   }
 
-  burst_avg = burstTotal/5;
+  // sort processes into descending order
+  sortProcessesByTime(p, NPROCESSES);
 
+/*
+  // prints either burst time or mem size of each processes
   for (idx = 0; idx <+ NPROCESSES; idx++) {
-    if ( p[idx].mem_size > 4000 )
-       P5.add(p[idx]);  // if mem size of process is > 4 GB, it has to go to processor 5
-    else {	// alternates processors and assigns an even number of processes to each processor
-        if ( counter == 1 ) {
-            P1.add(p[idx]);     // this is how you add processes to processor
-            counter = 2;
-        }
-        else if ( counter == 2 ) {
-        	P2.add(p[idx]);
-        	counter = 3;
-        }
-        else if ( counter == 3 ) {
-            P3.add(p[idx]);     // this is how you add processes to processor
-            counter = 4;
-        }
-        else {
-            P4.add(p[idx]);     // this is how you add processes to processor
-            counter = 1;
-        }
-    }
+    //cout << p[idx].burst_time << endl;
+    cout << p[idx].mem_size << endl;
   }
+*/
 
-  /*
-  for (idx = 0; idx <+ NPROCESSES; idx++) {
-    burstTotal += p[idx].burst_time;
-    cout << p[idx].burst_time << endl;
-  }*/
+  int smallCounter = 0, mediumCounter = 0, totalCounter = 0;
+  for (idx = 0; idx < NPROCESSES; idx++) {
+	  if ( p[idx].mem_size > 4000 ){
+		  P[4].add(p[idx]);  // if mem size of process is > 4 GB, it has to go to processor 5
+		  totalCounter++;
+	  }
+	  else if ( (p[idx].mem_size > 2000) && (p[idx].mem_size <= 4000) ) {
+		  // alternates processors and assigns an even number of processes to each processor
+		  if ( mediumCounter == 0 ) {
+			  P[2].add(p[idx]);
+			  mediumCounter = 1;
+			  totalCounter++;
+		  }
+		  else {
+			  P[3].add(p[idx]);
+			  mediumCounter = 0;
+			  totalCounter++;
+		  }
+	  }
+	  else {
+		  if ( smallCounter == 0 ) {
+			  P[0].add(p[idx]);
+			  smallCounter = 1;
+			  totalCounter++;
+		  }
+		  else {
+			  P[1].add(p[idx]);
+			  smallCounter = 0;
+			  totalCounter++;
+		  }
+	  }
+  } // end for
 
-  //burst_avg = burstTotal / 200;
-  //cout << endl << burst_avg << endl << endl;
-  P1.print(); P2.print(); P3.print(); P4.print(); P5.print();
+//  cout << "\n" << totalCounter << endl;	// says it reaches 200 but the total times of the processes dont add up to the total
+  	  	  	  	  	  	  	  	  	  	  	// time calculated in main??
+//  return 0;
+
+  cout << "Total turnaround time of all processes:    " << burstTotal;
+  burst_avg = burstTotal/NPROCESSORS;
+  cout << "\nOptimal turnaround time of each processor: " << burst_avg << endl;
+  cout << "-------------------------------------------------\n";
+  // print list of processes in each processor
+  int maxTime = 0;
+  for ( idx = 0; idx < NPROCESSORS; idx++ ) {
+	  P[idx].print();
+	  if ( maxTime < P[idx].getTotalTime() )  maxTime = P[idx].getTotalTime();
+  }
+  cout << "-------------------------------------------------\n";
+  float max = maxTime, burst = burst_avg;
+  float deviation = max/burst;
+  cout << "Longest Processor Time: " << maxTime << "    Percent difference: " << (deviation - 1) * 100 << "%";
 
   return 0;
 }
@@ -213,6 +258,24 @@ int findBurstTime() {
 double findMemSize() {
   return rand() % 799975 + 25 ; // number returned is divided by 100
                                 // to convert it back to MB
+}
+
+/*
+ * Uses in-place bubblesort of Process array using burst-time as basis for ordering.
+ * Returns original array in descending burst-time order
+ */
+void sortProcessesByTime( struct Process processArray[], int arraySize ) {
+	if ( arraySize < 2 )
+		return;
+
+    for ( int passCtr = 0; passCtr < arraySize; passCtr++ ) // loop arraySize times
+    	for ( int ndx = 0; ndx < arraySize ; ndx++ )
+    		// swap adjacent elts if in ascending order
+            if ( processArray[ndx].burst_time < processArray[ndx+1].burst_time ) {
+               struct Process tmpProcess = processArray[ndx];
+               processArray[ndx] = processArray[ndx+1];
+               processArray[ndx+1] = tmpProcess;
+            }
 }
 
 
